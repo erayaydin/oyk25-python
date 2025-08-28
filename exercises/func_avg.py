3cd4c4aacbd76c53ad11e90673df336a822e4141
Eray Aydın <erayaydinn@protonmail.com>
2025-08-28 12:29:32 +0300

    feat: add answers for avg and tax exercises

--- exercises/func_avg.py
+++ exercises/func_avg.py
@@ -3,4 +3,22 @@ Parametre olarak girilen tüm sayıların ortalamasını bulan
 fonksiyonu yazınız. Dönüş değeri float olmalı.
 """
 def ortalama(*sayilar):
-	pass
+	print(f"Bana gelen sayılar: {sayilar}")
+	# return sum(sayilar) / adet if (adet:=len(sayilar)) > 0 else 0.0
+	toplam = sum(sayilar)
+	for sayi in sayilar:
+		toplam += sayi
+	sayi_adeti = len(sayilar)
+	if sayi_adeti == 0:
+		return 0.0
+	ortalama_degeri = toplam / sayi_adeti
+	return ortalama_degeri
+
+deger1 = ortalama(1,2,3)
+print(f"{deger1=}")
+deger2 = ortalama(1,2,3,4,5,6)
+print(f"{deger2=}")
+deger3 = ortalama(1)
+print(f"{deger3=}")
+deger4 = ortalama()
+print(f"{deger4=}")
--- exercises/func_tax.py
+++ exercises/func_tax.py
@@ -3,4 +3,15 @@ Bir KDV hesaplama fonksiyonu yazınız.
 """
 
 def kdv_ekle(tutar, oran=0.18):
-	pass
+	kdvli_hali = tutar * (1 + oran)
+	return kdvli_hali
+
+deger1 = kdv_ekle(100)
+deger2 = kdv_ekle(130, oran=0.2)
+deger2 = kdv_ekle(130, oran=0.3)
+deger2 = kdv_ekle(130, oran=0.4)
+deger2 = kdv_ekle(190, oran=0.2)
+deger2 = kdv_ekle(200, oran=0.1)
+
+print(f"{deger1=}") # print(f"deger1={deger1}") # 118
+print(f"{deger2=}") # print(f"deger2={deger2}") # 156
