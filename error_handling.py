55e2580fe7787f5a3c4ab6ac9003d96a5ad9d7f7
Eray Aydın <erayaydinn@protonmail.com>
2025-08-29 17:02:24 +0300

    feat: add error handling main file

---
+++ error_handling.py
@@ -0,0 +1,28 @@
+"""
+def bolme(sayi, sayiya):
+    sayi / sayiya
+
+def deneme():
+    bolme(1, 0)
+
+deneme()
+"""
+
+"""
+try:
+    1/0
+    "1" + 1
+    print("bu yazmayacak")
+except (ZeroDivisionError, TypeError):
+    print("0'a bölme hatasında da buraya giriyor, TypeError hatasında da")
+except ZeroDivisionError as hata:
+    print("0'a bölme hatası aldım!")
+except TypeError:
+    print("Napıyon hacı.")
+except* BaseExceptionGroup:
+else:
+    pass
+finally:
+    print("benim işim bitti")
+"""
+
