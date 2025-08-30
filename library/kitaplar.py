536f4fd4daaaa5942744363298804b72af66d297
Eray Aydın <erayaydinn@protonmail.com>
2025-08-30 11:18:13 +0300

    feat: add answer for kitaplar

--- library/kitaplar.py
+++ library/kitaplar.py
@@ -7,24 +7,59 @@ def kitap_ekle(isim, sayfa=None, ogrenci=None):
 	"""
 	Kitaplar sözlüğüne yeni bir kitap ekler.
 	"""
-	pass
+	deger = { "sayfa": sayfa, "ogrenci": ogrenci }
+	kitaplar[isim] = deger
+	#kitaplar.update({ isim: deger })
+
+# kitap_ekle("Kitap 1", 100)
+# kitap_ekle("Kitap 2")
+# kitap_ekle("Kitap 3", ogrenci="Mustafa")
+# print(kitaplar)
+
+"""
+{
+	'Kitap 1': {
+		'sayfa': 100,
+		'ogrenci': None
+	},
+	'Kitap 2': {
+		'sayfa': None,
+		'ogrenci': None
+	},
+	'Kitap 3': {
+		'sayfa': None,
+		'ogrenci': 'Mustafa'
+	}
+}
+"""
 
 def kitap_ara(isim):
 	"""
 	Kitaplar sözlüğünde bir ismi arar ve eğer bulursa o sözlük
 	elemanını döndürür.
 	"""
-	pass
+	return kitaplar[isim] if isim in kitaplar else None
+	# if isim in kitaplar:
+	#	return kitaplar[isim]
+	#else:
+	#	return None
 
 def kitap_emanet(isim, ogrenci):
 	"""
-	Kitaplar sözlüğündeki elemanın 'ogrenci' anahtaırnı belirtilen
+	Kitaplar sözlüğündeki elemanın 'ogrenci' anahtarını belirtilen
 	öğrenci ile günceller
 	"""
-	pass
+	kitap = kitap_ara(isim)
+	# kitap["ogrenci"] = ogrenci
+	# kitaplar.update({ isim: kitap })	
+	if kitap:
+		kitap["ogrenci"] = ogrenci
 
+# kitap_emanet("Anna Karenina", "Mustafa")
+# kitap_emanet("Anna Karenina", None)
 def kitap_sil(isim):
 	"""
 	Kitaplar sözlüğündeki ilgili kitabı siler.
 	"""
-	pass
+	# del kitaplar[isim]
+	return kitaplar.pop(isim, None)
