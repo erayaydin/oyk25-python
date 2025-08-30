f7ae25038fe95da6dd8e00cf9e8fe41a1ed5e669
Eray Aydın <erayaydinn@protonmail.com>
2025-08-30 12:29:41 +0300

    feat: add ogrenciler answer

--- library/ogrenciler.py
+++ library/ogrenciler.py
@@ -22,31 +22,55 @@ def ogrenci_ekle(isim, sinif):
 	"""
 	Öğrenciler sözlüğüne yeni bir öğrenci ekler.
 	"""
-	pass
+	ogrenciler[isim] = { "isim": isim, "sinif": sinif, "kitap": None }
 
 def ogrenci_ara(isim):
 	"""
 	Öğrenciler sözlüğünde bir ismi arar ve eğer bulursa o sözlük
 	elemanını döndürür.
 	"""
-	pass
+	return ogrenciler[isim] if isim in ogrenciler else None
+	# if isim in ogrenciler:
+	#	return ogrenciler[isim]
+	#else
+	# 	return None
 
 def ogrenci_kitap_ekle(isim, kitap):
 	"""
 	Öğrenciler sözlüğündeki öğrenciyi bulup 'kitap' anahtarını ödünç
 	aldığı kitap ile değiştirir.
 	"""
-	pass
+	ogrenci = ogrenci_ara(isim)
+	if ogrenci:
+		ogrenci["kitap"] = kitap
 
-def ogrenci_kitap_sil(isim, kitap):
+def ogrenci_kitap_sil(isim):
 	"""
 	Öğrenciler sözlüğündeki öğrenciyi bulup 'kitap' anahtarını None olarak
 	değiştirir.
 	"""
-	pass
+	ogrenci_kitap_ekle(isim, None)
+
 
 def ogrenci_sil(isim):
 	"""
 	Öğrenciler sözlüğündeki öğrenciyi sözlükten siler.
 	"""
-	pass
+	return ogrenciler.pop(isim, None)
+
+if __name__ == "__main__":
+	ogrenci_ekle("Eray", "Python ile Programlamaya Giriş")
+
+	print(ogrenci_ara("Eray"))
+	print(ogrenci_ara("Mustafa"))
+
+	ogrenci_ekle("Mustafa", "Python ile Programlamaya Giriş")
+	print("Yeni öğrenci eklendi!", ogrenciler)
+	ogrenci_kitap_ekle("Mustafa", "Anna Karenina")
+	print("Mustafa kitap aldı", ogrenciler)
+	ogrenci_kitap_sil("Mustafa")
+	print("Mustafa kitabı geri verdi", ogrenciler)
+
+	print(ogrenciler)
+	ogrenci_sil("Eray")
+	print(ogrenciler)
