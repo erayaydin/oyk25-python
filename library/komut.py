2243adbb7c167023d7b9c3353944609715a0b1af
Eray Aydın <erayaydinn@protonmail.com>
2025-08-30 12:29:03 +0300

    feat: add komut answer

--- library/komut.py
+++ library/komut.py
@@ -8,7 +8,9 @@ def baslik(metin, karakter="#"):
 	# Başlık #
 	##########
 	"""
-	pass
+	print(karakter * (len(metin) + 4))
+	print(f"{karakter} {metin} {karakter}")
+	print(karakter * (len(metin) + 4))
 
 def listele_secim_al(liste):
 	"""
@@ -22,4 +24,30 @@ def listele_secim_al(liste):
 	yazar ve kullanıcıdan input bekler. Gelen input'a göre return
 	olarak "ekle" döndürür. Farklı bir input gelirse tekrar soru sorar.
 	"""
-	pass
+	anahtarlar = list(liste.keys())
+
+	for i, key in enumerate(anahtarlar, start=1):
+		print(f"{i}) {liste[key]}")
+	
+	while True:
+		secim = input("> ")
+		if not secim.isdigit():
+			print("Lütfen sayısal seçim yapınız.")
+			continue
+		secim = int(secim) # 1
+		if secim < 1 or secim > len(anahtarlar):
+			print(f"Lütfen 1 ve {len(anahtarlar)} arasında seçim yapın")
+			continue
+		break
+	# 1) Ekle, 2) Sil, 3) Cikis
+	# secim = 1 (ekle)
+	# anahtarlar = ["ekle", "sil", "cikis"]
+	return anahtarlar[secim-1]
+
+
+kullanici_secim = listele_secim_al({
+	"ekle": "Kullanıcı Ekle",
+	"sil": "Kullanıcı Sil",
+	"cikis": "Çıkış" }
+)
+print("Kullanıcı bunu seçti", kullanici_secim)
